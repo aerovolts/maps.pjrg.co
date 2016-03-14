234281480f68b16cf2492ae3e26fee1687110d9e
Patrick Guelcher <fortesp15@gmail.com>
2016-03-14 16:06:40 -0400

    Re-write index page
    As much as I like the other style, it is too messy for this area of the
    site.

--- main.py
+++ main.py
@@ -11,16 +11,22 @@ def home():
     description = 'The central index of web mapping projects created by Patrick Guelcher.'
     return render_template('pages/index.html', author=author, year=year, title=title, description=description)
 
+@app.route('/2016-election')
+def election_2016():
+    title = '2016 U.S. Election Results | Patrick Guelcher'
+    description = 'An index of all mapping projects focused on the 2016 U.S. Presidential Elections.'
+    return render_template('pages/2016-election.html', author=author, year=year, title=title, description=description)
+
 @app.route('/2016-election/republican-primaries')
 def republican_primaries_2016():
-    title = 'Republican Primary Results | 2016 Presidential Electon'
-    description = 'A web map displaying the results of the 2016 Republican Primaries prior to the 2016 General Election.'
+    title = 'Republican Primary Results | 2016 U.S. Presidential Electon'
+    description = 'A web map displaying the results of the 2016 U.S. Republican Primaries prior to the 2016 U.S. General Election.'
     return render_template('pages/2016-republican-primaries.html', author=author, year=year, title=title, description=description)
 
 @app.route('/2016-election/democratic-primaries')
 def democratic_primaries_2016():
-    title = 'Republican Primary Results | 2016 Presidential Electon'
-    description = 'A web map displaying the results of the 2016 Democratic Primaries prior to the 2016 General Election.'
+    title = 'Republican Primary Results | 2016 U.S. Presidential Electon'
+    description = 'A web map displaying the results of the 2016 U.S. Democratic Primaries prior to the 2016 U.S. General Election.'
     return render_template('pages/2016-democratic-primaries.html', author=author, year=year, title=title, description=description)
 
 if __name__ == '__main__':
